4d4162b9fb131abf2814791b681ef9c1c0928744
Alexander <120810491+Alexander4409@users.noreply.github.com>
2024-11-08 11:05:21 +0700

    test_commit

---
+++ Tailakov.A.A_HW_search_and_sort_Part_2.py
@@ -0,0 +1,49 @@
+#Task 1
+# users_phone = [89233165316, 89223165016, 89231165026, 89233165017 ]
+# users_id = [1,2,3,4]
+# users_guide_dict = {}
+# for i in range(0,len(users_id)):
+#     users_guide_dict[users_id[i]] = users_phone[i]
+#
+#
+#
+# while True:
+#     command = int (input("Choose the command: 1 - sort by phone number, 2 - sort by id, 3 - show all contacts, "
+#                          "4 - exit from program "))
+#     if command == 1:
+#         print(sorted(users_guide_dict.values()))
+#     elif command == 2:
+#         print(sorted(users_guide_dict.keys()))
+#     elif command == 3:
+#         print(users_guide_dict.items())
+#     elif command == 4:
+#         break
+#
+#     else:
+#         print("Unknown command program stopped!")
+#         break
+
+#Task 2
+# users_books = ["Clinical Microbiology", "Medical Microbiology & Immunology", "Microbiology Laboratory Theory ", "Bioinformatics and Functional Genomics"]
+# books_year = [2012, 2008, 1987, 2022]
+# users_library_dict = {}
+# for i in range(0,len(books_year)):
+#     users_library_dict[books_year[i]] = users_books[i]
+#
+# while True:
+#     command = int (input("Choose the command: 1 - sort by name books, 2 - sort by books year, 3 - show all books, "
+#                          "4 - exit from program "))
+#     if command == 1:
+#         print(sorted(users_library_dict.values()))
+#     elif command == 2:
+#         print(sorted(users_library_dict.keys()))
+#     elif command == 3:
+#         print(users_library_dict.items())
+#     elif command == 4:
+#         break
+#
+#     else:
+#         print("Unknown command program stopped!")
+#         break
+
+
